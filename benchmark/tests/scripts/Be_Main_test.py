b3bc2fbd2f86e9949cc7b3e26b522ec5c55b617c
Ricardo Montañana <rmontanana@gmail.com>
2022-05-09 00:23:18 +0200

    Complete be_main tests

--- benchmark/tests/scripts/Be_Main_test.py
+++ benchmark/tests/scripts/Be_Main_test.py
@@ -16,64 +16,102 @@ class BeMainTest(TestBase):
         self.remove_files(self.files, ".")
         return super().tearDown()
 
+    def check_output_lines(self, stdout, file_name, lines_to_compare):
+        with open(os.path.join(self.test_files, f"{file_name}.test")) as f:
+            expected = f.read()
+        computed_data = stdout.getvalue().splitlines()
+        n_line = 0
+        # compare only report lines without date, time, duration...
+        for expected, computed in zip(expected.splitlines(), computed_data):
+            if n_line in lines_to_compare:
+                self.assertEqual(computed, expected, n_line)
+            n_line += 1
+
     def test_be_benchmark_dataset(self):
         stdout, _ = self.execute_script(
             "be_main",
             ["-m", "STree", "-d", "balloons", "--title", "test"],
         )
-        with open(os.path.join(self.test_files, "be_main_dataset.test")) as f:
-            expected = f.read()
-        n_line = 0
-        # compare only report lines without date, time, duration...
-        lines_to_compare = [0, 2, 3, 5, 6, 7, 8, 9, 11, 12, 13]
-        for expected, computed in zip(
-            expected.splitlines(), stdout.getvalue().splitlines()
-        ):
-            if n_line in lines_to_compare:
-                self.assertEqual(computed, expected, n_line)
-            n_line += 1
+        self.check_output_lines(
+            stdout=stdout,
+            file_name="be_main_dataset",
+            lines_to_compare=[0, 2, 3, 5, 6, 7, 8, 9, 11, 12, 13],
+        )
 
     def test_be_benchmark_complete(self):
         stdout, _ = self.execute_script(
             "be_main",
             ["-s", self.score, "-m", "STree", "--title", "test", "-r", "1"],
         )
-        with open(os.path.join(self.test_files, "be_main_complete.test")) as f:
-            expected = f.read()
-        n_line = 0
         # keep the report name to delete it after
-        self.files.append(stdout.getvalue().splitlines()[-1].split("in ")[1])
-        # compare only report lines without date, time, duration...
-        lines_to_compare = [0, 2, 3, 5, 6, 7, 8, 9, 12, 13, 14]
-        for expected, computed in zip(
-            expected.splitlines(), stdout.getvalue().splitlines()
-        ):
-            if n_line in lines_to_compare:
-                self.assertEqual(computed, expected, n_line)
-            n_line += 1
+        report_name = stdout.getvalue().splitlines()[-1].split("in ")[1]
+        self.files.append(report_name)
+        self.check_output_lines(
+            stdout, "be_main_complete", [0, 2, 3, 5, 6, 7, 8, 9, 12, 13, 14]
+        )
 
     def test_be_benchmark_no_report(self):
         stdout, _ = self.execute_script(
             "be_main",
             ["-s", self.score, "-m", "STree", "--title", "test"],
         )
-        with open(os.path.join(self.test_files, "be_main_complete.test")) as f:
-            expected = f.read()
         # keep the report name to delete it after
         report_name = stdout.getvalue().splitlines()[-1].split("in ")[1]
         self.files.append(report_name)
         report = Report(file_name=report_name)
         with patch(self.output, new=StringIO()) as stdout:
             report.report()
-        # compare only report lines without date, time, duration...
-        lines_to_compare = [0, 2, 3, 5, 6, 7, 8, 9, 12, 13, 14]
-        n_line = 0
-        for expected, computed in zip(
-            expected.splitlines(), stdout.getvalue().splitlines()
-        ):
-            if n_line in lines_to_compare:
-                self.assertEqual(computed, expected, n_line)
-            n_line += 1
+        self.check_output_lines(
+            stdout,
+            "be_main_complete",
+            [0, 2, 3, 5, 6, 7, 8, 9, 12, 13, 14],
+        )
+
+    def test_be_benchmark_best_params(self):
+        stdout, _ = self.execute_script(
+            "be_main",
+            [
+                "-s",
+                self.score,
+                "-m",
+                "STree",
+                "--title",
+                "test",
+                "-f",
+                "1",
+                "-r",
+                "1",
+            ],
+        )
+        # keep the report name to delete it after
+        report_name = stdout.getvalue().splitlines()[-1].split("in ")[1]
+        self.files.append(report_name)
+        self.check_output_lines(
+            stdout, "be_main_best", [0, 2, 3, 5, 6, 7, 8, 9, 12, 13, 14]
+        )
+
+    def test_be_benchmark_grid_params(self):
+        stdout, _ = self.execute_script(
+            "be_main",
+            [
+                "-s",
+                self.score,
+                "-m",
+                "STree",
+                "--title",
+                "test",
+                "-g",
+                "1",
+                "-r",
+                "1",
+            ],
+        )
+        # keep the report name to delete it after
+        report_name = stdout.getvalue().splitlines()[-1].split("in ")[1]
+        self.files.append(report_name)
+        self.check_output_lines(
+            stdout, "be_main_grid", [0, 2, 3, 5, 6, 7, 8, 9, 12, 13, 14]
+        )
 
     def test_be_benchmark_no_data(self):
         stdout, _ = self.execute_script(
