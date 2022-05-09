a649efde735488c440e9092e15b60c32de5645d8
Ricardo Montañana <rmontanana@gmail.com>
2022-05-09 16:27:37 +0200

    Fix be_print_strees issues

--- benchmark/scripts/be_print_strees.py
+++ benchmark/scripts/be_print_strees.py
@@ -16,13 +16,13 @@ def load_hyperparams(score_name, model_name):
         return json.load(f)
 
 
-# def hyperparam_filter(hyperparams):
-#     res = {}
-#     for key, value in hyperparams.items():
-#         if key.startswith("base_estimator"):
-#             newkey = key.split("__")[1]
-#             res[newkey] = value
-#     return res
+def hyperparam_filter(hyperparams):
+    res = {}
+    for key, value in hyperparams.items():
+        if key.startswith("base_estimator"):
+            newkey = key.split("__")[1]
+            res[newkey] = value
+    return res
 
 
 def build_title(dataset, accuracy, n_samples, n_features, n_classes, nodes):
@@ -76,14 +76,16 @@ def main(args_test=None):
     arguments = Arguments()
     arguments.xset("color").xset("dataset", default="all").xset("quiet")
     args = arguments.parse(args_test)
-    hyperparameters = load_hyperparams("accuracy", "STree")
+    hyperparameters = load_hyperparams("accuracy", "ODTE")
     random_state = 57
     dt = Datasets()
     for dataset in dt:
         if dataset == args.dataset or args.dataset == "all":
             X, y = dt.load(dataset)
             clf = Stree(random_state=random_state)
-            hyperparams_dataset = hyperparameters[dataset][1]
+            hyperparams_dataset = hyperparam_filter(
+                hyperparameters[dataset][1]
+            )
             clf.set_params(**hyperparams_dataset)
             clf.fit(X, y)
             print_stree(clf, dataset, X, y, args.color, args.quiet)
